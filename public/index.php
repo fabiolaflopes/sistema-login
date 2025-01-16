82a6fc19ecda1249d963d279a8f7addfdc6127cf
Fabiola Ferreira <fabiola.lopes@futcorp.com.br>
2025-01-16 11:19:27 -0300

    usuarios-login

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// iniciar a sessão
+session_start();
+
+// definir uma constante de controle
+define('CONTROL', true);
+
+// verificar se existe um usuário logado
+$usuario_logado = $_SESSION['usuario'] ?? null;
+
+// verifica qual é a rota na URL
+if(empty($usuario_logado)){
+    $rota = 'login';
+} else {
+    $rota = $_GET['rota'] ?? 'home';
+}
+
+// analisa a rota
+$rotas = [
+    'login' => 'login.php',
+    'home' => 'home.php'
+];
